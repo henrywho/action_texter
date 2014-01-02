f593d497f5a68bf918967144d48f60f85c19a80e
Henry Ho <henry@avvo.com>
2014-01-02 14:18:22 -0800

    Make it backwards compatible.

--- lib/action_texter/twilio_delivery.rb
+++ lib/action_texter/twilio_delivery.rb
@@ -1,17 +1,33 @@
 module ActionTexter
   class TwilioDelivery
-    attr_reader :client
+    attr_reader :account
 
     def initialize(config = {})
-      sid = config[:account_sid]
-      token = config[:account_token]
-      raise ArgumentError, "you must specify config.action_texter.twilio_settings to contain a :account_sid" unless sid
-      raise ArgumentError, "you must specify config.action_texter.twilio_settings to contain a :account_token" unless token
-      @client = Twilio::REST::Client.new(sid, token)
+      if config[:sid] || config[:token]
+        # use old config parameters
+        sid = config[:sid]
+        token = config[:token]
+        subaccount = config[:subaccount]
+        raise ArgumentError, "you must specify config.action_texter.twilio_settings to contain a :sid" unless sid
+        raise ArgumentError, "you must specify config.action_texter.twilio_settings to contain a :token" unless token
+        client = Twilio::REST::Client.new(sid, token)
+        if subaccount
+          @account = client.accounts.find(subaccount)
+        else
+          @account = client.account
+        end
+      else
+        # use new config parameters
+        sid = config[:account_sid]
+        token = config[:account_token]
+        raise ArgumentError, "you must specify config.action_texter.twilio_settings to contain a :account_sid" unless sid
+        raise ArgumentError, "you must specify config.action_texter.twilio_settings to contain a :account_token" unless token
+        @account = Twilio::REST::Client.new(sid, token).account
+      end
     end
 
     def deliver(message)
-      client.account.messages.create(
+      account.messages.create(
         :from => message.from,
         :to => message.to,
         :body => message.body.strip
