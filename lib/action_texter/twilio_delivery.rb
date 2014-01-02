10f14807a84949e290075e8ab8cbeb9feb626e02
Henry Ho <henry@avvo.com>
2014-01-02 02:58:51 -0800

    Fix sending text from main Twilio account. Stop supporting subaccount. Rename config parameters.

--- lib/action_texter/twilio_delivery.rb
+++ lib/action_texter/twilio_delivery.rb
@@ -3,17 +3,15 @@ module ActionTexter
     attr_reader :client
 
     def initialize(config = {})
-      @sid = config[:sid]
-      @token = config[:token]
-      @subaccount = config[:subaccount]
-      raise ArgumentError, "you must specify config.action_texter.twilio_settings to contain a :sid" unless @sid
-      raise ArgumentError, "you must specify config.action_texter.twilio_settings to contain a :token" unless @token
-      @client = Twilio::REST::Client.new(@sid, @token)
-      @client = client.accounts.find(@subaccount) if @subaccount
+      sid = config[:account_sid]
+      token = config[:account_token]
+      raise ArgumentError, "you must specify config.action_texter.twilio_settings to contain a :account_sid" unless sid
+      raise ArgumentError, "you must specify config.action_texter.twilio_settings to contain a :account_token" unless token
+      @client = Twilio::REST::Client.new(sid, token)
     end
 
     def deliver(message)
-      client.sms.messages.create(
+      client.account.messages.create(
         :from => message.from,
         :to => message.to,
         :body => message.body.strip
